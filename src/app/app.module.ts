c6ff3ebe332f98e8a53e8d602ce9f2c6e11a2682
lm <leo.mangutov@gmail.com>
2019-04-09 16:12:54 -0700

    add chat components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { masterFirebaseConfig } from './api-keys';
+
+
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { routing } from './app.routing';
 import { GameRoomComponent } from './game-room/game-room.component';
 import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireDatabaseModule, FirebaseListObservable } from 'angularfire2/database';
+import { ChatFormComponent } from './chat-form/chat-form.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
+import { FeedComponent } from './feed/feed.component';
+import { MessageComponent } from './message/message.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
+
+import { ChatService } from './services/chat.service';
+
+import { appRoutes } from '../routes';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -21,17 +35,24 @@ export const firebaseConfig = {
   declarations: [
     AppComponent,
     LandingComponent,
-    GameRoomComponent
+    GameRoomComponent,
+    ChatFormComponent,
+    ChatroomComponent,
+    FeedComponent,
+    MessageComponent,
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule,
     HttpModule,
     routing,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ChatroomComponent } from './app/chatroom/chatroom.component';
+
+export const appRoutes: Routes = [
+  {
+    path: 'chat',
+    component: ChatroomComponent
+  }
+];
