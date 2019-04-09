68159519426ddc0c84bc3e7396e938ecad56d56d
Student <example@example.com>
2019-04-09 15:54:29 -0700

    Room Generator is own component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component'
 import { GameRoomComponent } from './game-room/game-room.component'
+import { AuthenticationComponent } from './authentication/authentication.component'
 
 const appRoutes: Routes = [
   {
@@ -9,9 +10,13 @@ const appRoutes: Routes = [
     component: LandingComponent
   },
   {
-  path: 'rooms/:id',
-  component: GameRoomComponent
-}
+    path: 'rooms/:id',
+    component: GameRoomComponent
+  },
+  {
+    path: 'signIn',
+    component: AuthenticationComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
