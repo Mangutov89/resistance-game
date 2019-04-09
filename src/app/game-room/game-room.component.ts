3257f0f2f6c1d4652f04be1b26ce76671eebc218
Joe Pritchett <joempritchett@gmail.com>
2019-04-09 11:34:42 -0700

    add code to generate a unique room and take the user to it in one click

--- src/app/game-room/game-room.component.ts
+++ src/app/game-room/game-room.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Game } from '../models/game.model'
+import { RoomGeneratorService } from '../room-generator.service'
 
 @Component({
   selector: 'app-game-room',
   templateUrl: './game-room.component.html',
-  styleUrls: ['./game-room.component.css']
+  styleUrls: ['./game-room.component.css'],
+  providers: [ RoomGeneratorService ]
 })
 export class GameRoomComponent implements OnInit {
+  roomId: string;
+  roomToDisplay;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location, private roomService: RoomGeneratorService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.roomId = urlParameters['id'];
+    });
+    this.roomToDisplay = this.roomService.getRoomById(this.roomId);
+    console.log(this.roomToDisplay.chat)
+    console.log(this.roomToDisplay)
   }
-
 }
