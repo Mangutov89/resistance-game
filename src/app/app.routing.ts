f747779577da8b5c8f7efe29a51779cfa2af7691
Student <example@example.com>
2019-04-10 09:41:49 -0700

    Import statements fixed, combined routing files

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,15 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LandingComponent } from './landing/landing.component'
-import { GameRoomComponent } from './game-room/game-room.component'
-import { AuthenticationComponent } from './authentication/authentication.component'
+import { LandingComponent } from './landing/landing.component';
+import { GameRoomComponent } from './game-room/game-room.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     redirectTo: '/welcome',
     pathMatch: 'full'
-  },
+  }
   {
     path: 'rooms/:id',
     component: GameRoomComponent
@@ -17,11 +18,11 @@ const appRoutes: Routes = [
   {
     path: 'welcome',
     component: LandingComponent,
-    children: [
-      { path: "signIn", component: AuthenticationComponent},
-    ]
+  },
+  {
+    path: 'chat',
+    component: ChatroomComponent
   }
-
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
